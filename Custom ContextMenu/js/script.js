329f0ba2aafab3c4c8dabe103cdc1bda57f91a13
Aliakbargh85 <131409388+Aliakbargh85@users.noreply.github.com>
2025-10-30 21:04:09 +0330

    Add files via upload

---
+++ Custom ContextMenu/js/script.js
@@ -0,0 +1,32 @@
+﻿let $ = document;
+
+const contextMenu = $.getElementById("contextMenu");
+
+function contextHandler(e) {
+  e.preventDefault();
+
+  if (contextMenu.style.display === "none") {
+    contextMenu.style.left = e.pageX + "px";
+    contextMenu.style.top = e.pageY + "px";
+    contextMenu.style.display = "block";
+  } else {
+    contextMenu.style.left = e.pageX + "px";
+    contextMenu.style.top = e.pageY + "px";
+  }
+
+  console.log(e);
+}
+
+function clickContextMenu() {
+  contextMenu.style.display = "none";
+}
+
+function keyDownHandler(e) {
+  if (e.key === "Escape") {
+    contextMenu.style.display = "none";
+  }
+}
+
+$.body.addEventListener("contextmenu", contextHandler);
+$.body.addEventListener("click", clickContextMenu);
+$.body.addEventListener("keydown", keyDownHandler);
